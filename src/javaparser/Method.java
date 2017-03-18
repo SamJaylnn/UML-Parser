ddc552fa0acd5db8be36ed1fa1a2e6df1efa7637
SamJaylnn <chenshuzhongs@gmail.com>
2017-03-17 17:24:37 -0700

    configure Method class

---
+++ src/javaparser/Method.java
@@ -0,0 +1,36 @@
+package javaparser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.javaparser.ast.TypeParameter;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.type.ReferenceType;
+import com.github.javaparser.ast.type.Type;
+
+public class Method {
+	public String methodName;
+	public NameExpr methodNameExpr;
+	public List<Parameter> methodParameters;
+	public List<TypeParameter> methodTypeParameters;
+	public List<ReferenceType> methodThrows;
+	public Type methodType;
+	
+	public Method() {
+		methodName = new String();
+		methodNameExpr = new NameExpr();
+		methodParameters = new ArrayList<Parameter>();
+		methodTypeParameters = new ArrayList<TypeParameter>();
+		methodThrows = new ArrayList<ReferenceType>();		
+	}
+}
+
+/* for reference
+String	getName() 
+NameExpr	getNameExpr() 
+List<Parameter>	getParameters() 
+List<ReferenceType>	getThrows() 
+Type	getType() 
+List<TypeParameter>	getTypeParameters() 
+*/
