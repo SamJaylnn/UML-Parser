3d2f0fd297e2185e3f9a0cade8f78774b23bec70
SamJaylnn <chenshuzhongs@gmail.com>
2017-04-04 16:07:05 -0700

    attribut modify 1.0

--- src/javaparser/AttributeVisitor.java
+++ src/javaparser/AttributeVisitor.java
@@ -3,9 +3,11 @@ package javaparser;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.expr.VariableDeclarationExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.ast.type.Type;
 
 class AttributeVisitor extends VoidVisitorAdapter{
 		public List<Attribute> attributeList = new ArrayList<Attribute>();
@@ -13,15 +15,21 @@ class AttributeVisitor extends VoidVisitorAdapter{
 		// Override the visit function
 		public AttributeVisitor() {}
         @Override
-        public void visit(VariableDeclarationExpr v, Object arg) {
-        	 List <VariableDeclarator> myVars = v.getVars();
-             for (VariableDeclarator vars: myVars){
+        public void visit(FieldDeclaration v, Object arg) {
+        	 Type type = v.getType();
+        	 String modifier = ModifierSet.getAccessSpecifier(v.getModifiers()).toString();
+             for (Node vars: v.getChildrenNodes()){
             	 Attribute attribute = new Attribute();
-            	 attribute.attributeName = vars.getId().getName();
-            	 attribute.attributeName = vars.
+            	 attribute.attributeName = ((VariableDeclarator)vars).getId().getName();
+            	 attribute.type = type;
+            	 attribute.modifier = modifier;
             	 attributeList.add(attribute);
+            	 
+            	 /***** test *****/
+            	 System.out.println("name:" + attribute.attributeName);
+            	 System.out.println("type:" + attribute.type);
+            	 System.out.println("modifier:" + attribute.modifier);
+            	 System.out.println(" ");
              }
-             
-        	
         }
     }
