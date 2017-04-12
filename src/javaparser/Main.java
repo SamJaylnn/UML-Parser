759829f910360f2c80a859779f30ce586e36790e
SamJaylnn <chenshuzhongs@gmail.com>
2017-04-11 21:04:39 -0700

    create jar

--- src/javaparser/Main.java
+++ src/javaparser/Main.java
@@ -7,20 +7,21 @@ import net.sourceforge.plantuml.SourceStringReader;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		args= new String[2];
-		//Scanner reader = new Scanner(System.in);  // Reading from System.in
-		System.out.println("Enter a folder path: ");
+		//args= new String[2];
+		//Scanner reader = new Scanner(System.in);
 		//args[0] = reader.next();
-		System.out.println("Enter a output file name: ");
 		//args[1] = reader.next();
-		
+		if (args.length == 2) {
+			UMLparser parser = new UMLparser(args[0], args[1]);
+			parser.analyze();	
+		}
 		/* test case 1 */
-		for (int i = 1; i <= 5; i++) {
+		/*for (int i = 1; i <= 5; i++) {
 			args[0] = "uml_cases/uml-parser-test-" + Integer.toString(i);
 			args[1] = "uml" + Integer.toString(i) + ".png";
 			
 			UMLparser parser = new UMLparser(args[0], args[1]);
 			parser.analyze();
-		}
+		}*/
 	}
 }
