4fed5e303df5c72981cbea4d1a42739e3e4f866e
SamJaylnn <chenshuzhongs@gmail.com>
2017-04-15 18:39:08 -0700

    input format modifide

--- src/javaparser/Main.java
+++ src/javaparser/Main.java
@@ -7,10 +7,7 @@ import net.sourceforge.plantuml.SourceStringReader;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		//args= new String[2];
-		//Scanner reader = new Scanner(System.in);
-		//args[0] = reader.next();
-		//args[1] = reader.next();
+
 		if (args.length == 2) {
 			UMLparser parser = new UMLparser(args[0], args[1]);
 			parser.analyze();	
