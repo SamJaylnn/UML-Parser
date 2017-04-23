0c8ccb7ab300ab82f6da923132da0ed40fd750d2
SamJaylnn <chenshuzhongs@gmail.com>
2017-04-22 18:15:58 -0700

    comment modified (class CoIVisitor)

--- src/javaparser/CoIVisitor.java
+++ src/javaparser/CoIVisitor.java
@@ -10,15 +10,7 @@ import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.*;
 
 
-/*	for reference:
- *	String	getName() 
-	NameExpr	getNameExpr() 	
-	List<BodyDeclaration>	getMembers() 
-	List<ClassOrInterfaceType>	getExtends() 
-	List<ClassOrInterfaceType>	getImplements() 
-	List<TypeParameter>	getTypeParameters() 
-	boolean	isInterface() 
-*/
+
 public class CoIVisitor extends VoidVisitorAdapter{
 		public CoI coi = new CoI();
 		
