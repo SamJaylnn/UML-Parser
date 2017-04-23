bf7e061a15f7186017b004f4755d0287ccf57c84
SamJaylnn <chenshuzhongs@gmail.com>
2017-04-22 17:25:25 -0700

    shell script initial created

---
+++ umlparser.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+arg1=$1
+arg2=$2
+
+##directory where jar file is located    
+dir=E:/javaworkspace/UMLparser
+
+##jar file name
+jar_name=umlparser.jar
+
+## Permform some validation on input arguments, one example below
+if [ -z "$1" ] || [ -z "$2" ]; then
+        echo "Missing arguments, exiting.."
+        echo "Usage : $0 arg1 arg2"
+        exit 1
+fi
+
+java -jar $dir/$jar_name arg1 arg2
